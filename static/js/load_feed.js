2fb0ed5ae53fcb29455b858af847f50f1ab2a12e
ReptileSD <denbakushev@gmail.com>
2020-05-01 12:29:25 +1000

    load_feed v.1.2

--- static/js/load_feed.js
+++ static/js/load_feed.js
@@ -4,15 +4,5 @@ var last_id = last_post.db_id;
 delete last_post.id;
 
 var new_posts = jQuery.ajax('localhost:5000/api/v01/posts/' + last_id); //todo: Ilyaaaaa help
-new_posts = JSON.parse(new_posts)
 
-new_posts.forEach(
-    function(item, i, arr) // item - элемент, i - номер, arr - перебираемый массив
-    {
-        var t = render_template(template, item); //todo: Ilyaaaaa help
-        if (item == arr[arr.length - 1])
-        {
-            t.id = "last";
-        }
-        element.innerHTML = element.innerHTML + t; //innerhtml - то что написано внутри тега
-    });
+element.innerHTML = element.innerHTML + new_posts; //innerhtml - то что написано внутри тега
