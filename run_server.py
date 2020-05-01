e52216165b7ab5e16e9f364583ef9b8128646c4f
FroggyKwa <ilya.laguxa@gmail.com>
2020-05-01 18:45:12 +1000

    edit port

--- run_server.py
+++ run_server.py
@@ -8,4 +8,4 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(blueprint)
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='localhost', port=80)
