7450d05eb1311c272258e15ba6fb5f19c7c446a5
ReptileSD <denbakushev@gmail.com>
2020-05-01 14:24:04 +1000

    load_feed v.1.4

--- static/js/load_feed.js
+++ static/js/load_feed.js
@@ -1,9 +1,24 @@
-var element = document.getElementById("feed");
-var last_post = element.getElementById("last");
-var last_id = last_post.db_id;
-delete last_post.id;
+function load_feed()
+{
+    var element = document.getElementById("feed");
+    var last_post = document.getElementById("last");
+    if (last_post != null)
+    {
+        var last_id = last_post.db_id;
+        delete last_post.id;
+    }
+    else
+    {
+        var last_id = '10';
+    }
 
-var new_posts = jQuery.ajax('localhost:5000/api/v01/posts/' + last_id); //todo: Ilyaaaaa help
-new_posts = JSON.parse(new_posts)
+    var btn = document.getElementById("btn");
+    delete btn;
 
-element.innerHTML = element.innerHTML + new_posts; //innerhtml - то что написано внутри тега
+    var new_posts = jQuery.ajax('/api/v01/posts/' + last_id); //todo: Ilyaaaaa help
+    //new_posts = JSON.parse(new_posts);
+    alert(new_posts)
+
+    element.innerHTML = element.innerHTML + new_posts["message"]; //innerhtml - то что написано внутри тега
+    element.innerHTML = element.innerHTML + "<button id='btn' onclick='load_feed()'>Загрузить еще</button>";
+}
--- static/js/update_feed.js
+++ static/js/update_feed.js
@@ -1,10 +1,12 @@
-var element = document.getElementById("feed");
+function update_feed(){
+    var element = document.getElementById("feed");
 
-var new_posts = jQuery.ajax(url1? settings?);
+    //var new_posts = jQuery.ajax(url1? settings?);
 
-new_posts.forEach(
-    function(item, i, arr) // item - элемент, i - номер, arr - перебираемый массив
-    {
-        var t = render_template(template, item);
-        element.innerHTML = t + element.innerHTML;
-    });
+    new_posts.forEach(
+        function(item, i, arr) // item - элемент, i - номер, arr - перебираемый массив
+        {
+            var t = render_template(template, item);
+            element.innerHTML = t + element.innerHTML;
+        });
+}
