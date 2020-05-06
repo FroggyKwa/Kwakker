8465fe2c93aeee49c4efd4bf8f813ef05216e3b1
ReptileSD <denbakushev@gmail.com>
2020-05-06 22:20:12 +1000

    load_feed v1.9

--- static/js/load_feed.js
+++ static/js/load_feed.js
@@ -1,21 +1,27 @@
-function load_feed()
+function load_feed(tags=[])
 {
     var element = document.getElementById("feed");
     var last_post = document.getElementById("last");
-    if (last_post != null)
+    /*if (last_post != null)
     {
-        var last_id = last_post.db_id;
-
+        var last_id = last_post.getAttribute('db_id');
     }
     else
     {
         var last_id = '-1';
+    }*/
+    if (last_post == null) //поменять на undefined есчо
+    {
+        last_id = "";
     }
-
-    var new_posts = jQuery.ajax('/api/v01/posts/' + last_id, {
+    else
+    {
+        last_id = 'post_id=' + last_post.getAttribute('db_id');
+    }
+    var new_posts = jQuery.ajax('/api/v01/posts?' + 'tags=' + tags + '&' + last_id, {
         success: function(){
                                 element.innerHTML = element.innerHTML + new_posts.responseJSON; //innerhtml - то что написано внутри тега
                                 last_post.id = '';
                             }
-    }); //todo: Ilyaaaaa help
+    });
     }
