c2975851baa8dd83dd3ec35cbc70f30fab1d456a
FroggyKwa <ilya.laguxa@gmail.com>
2020-05-08 00:38:42 +1000

    fix search

--- data/tags.py
+++ data/tags.py
@@ -9,7 +9,7 @@ from .db_session import SqlAlchemyBase
 class Tag(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'tag'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    content = sqlalchemy.Column(sqlalchemy.String(collation='NOCASE'), nullable=True)
     created_at = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
 
 
