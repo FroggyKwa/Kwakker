bee7944c40c15f1ade23ca7dc94fb4c2020d558b
ReptileSD <denbakushev@gmail.com>
2020-05-08 16:06:11 +1000

    load_feed v2.0

--- static/js/load_feed.js
+++ static/js/load_feed.js
@@ -2,30 +2,25 @@ function load_feed(tags=[])
 {
     var element = document.getElementById("feed");
     var last_post = document.getElementById("last");
-    console.log(element);
     if (last_post == null)
     {
-        last_id = "";
+        var last_id = "";
     }
     else
     {
-        last_id = '&post_id=' + last_post.getAttribute('db_id');
+        var last_id = '&post_id=' + last_post.getAttribute('db_id');
     }
     var new_posts = jQuery.ajax('/api/v01/posts?' + 'tag=' + tags + last_id, {
         success: function(){
-          console.log(Boolean(tags));
-          if (tags == false)
+          $(last_post).removeAttr("id");
+          element.innerHTML = element.innerHTML + new_posts.responseJSON;
+          last_post = document.getElementById('last')
+          last_id = last_post.getAttribute('db_id');
+          if (last_id == "1")
           {
-            $(last_post).removeAttr("id");
-            element.innerHTML = element.innerHTML + new_posts.responseJSON;
-            $(last_post).remove()
+              var btn = document.getElementById("btn");
+              btn.parentNode.removeChild(btn);
           }
-          else
-          {
-            element.innerHTML = new_posts.responseJSON;
-          }
-
-                                //innerhtml - то что написано внутри тега
                             }
     });
     }
